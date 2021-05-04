37f31ce2c956f750a97a226d0030253b8ef48a60
Christopher Spalatin <christopherspalatin@Christophers-iMac.local>
2021-05-04 16:42:38 -0500

    api with Ed

--- api/index.js
+++ api/index.js
@@ -1,34 +1,47 @@
 // Build an apiRouter using express Router
-
+const apiRouter = require("express").Router();
 
 // Import the database adapter functions from the db
-
-
+const { getOpenReports, createReport } = require("../db");
 /**
  * Set up a GET request for /reports
- * 
+ *
  * - it should use an async function
  * - it should await a call to getOpenReports
  * - on success, it should send back an object like { reports: theReports }
  * - on caught error, call next(error)
  */
+apiRouter.get("/reports", async (req, res, next) => {
+  try {
+    const openReports = await getOpenReports();
 
-
-
+    res.send({
+      reports: openReports,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 /**
  * Set up a POST request for /reports
- * 
+ *
  * - it should use an async function
  * - it should await a call to createReport, passing in the fields from req.body
  * - on success, it should send back the object returned by createReport
  * - on caught error, call next(error)
  */
+apiRouter.post("/reports", async (req, res, next) => {
+  try {
+    const newReport = await createReport(req.body);
 
-
-
+    res.send(newReport);
+  } catch (error) {
+    next(error);
+  }
+});
 /**
  * Set up a DELETE request for /reports/:reportId
- * 
+ *
  * - it should use an async function
  * - it should await a call to closeReport, passing in the reportId from req.params
  *   and the password from req.body
@@ -36,11 +49,9 @@
  * - on caught error, call next(error)
  */
 
-
-
 /**
  * Set up a POST request for /reports/:reportId/comments
- * 
+ *
  * - it should use an async function
  * - it should await a call to createReportComment, passing in the reportId and
  *   the fields from req.body
@@ -48,6 +59,5 @@
  * - on caught error, call next(error)
  */
 
-
-
 // Export the apiRouter
+module.exports = apiRouter;
